f075bcba6b08347100a3c1f6aeb396f7826c66e9
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-08-31 19:35:32 -0700

    added logos and text from NavOptions.js

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, FlatList, Image } from "react-native";
+import React from "react";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
 
 const NavOptions = () => {
   return (
-    <View>
-      <Text>Hello world</Text>
-    </View>
-  )
-}
+    <FlatList
+      data={data}
+      horizontal
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <TouchableOpacity>
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <Text>{item.title}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
 
-export default NavOptions
+export default NavOptions;
