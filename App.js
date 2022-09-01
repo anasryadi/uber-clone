dab3b55a1948d148bf1cbb8c9f60cb1e0fdf6c2b
Anas Ryadi <anasryadi@Anass-MBP-M1.socal.rr.com>
2022-09-01 00:13:47 -0700

    fixed the npm installs and created MapScreen.js

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+// import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
+// import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 
 // 1) Set up redux
 // 2)
@@ -11,9 +14,11 @@ import { store } from "./store";
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
